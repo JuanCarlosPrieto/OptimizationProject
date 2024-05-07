d9c8ed87620142771f70989a9d07c238cee1ea6a
OMICRON1123 <jprietoc@unal.edu.co>
2024-05-07 15:35:37 -0500

    Primera prueba de funcionamiento

--- scrapping.py
+++ scrapping.py
@@ -1,1 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+driver = webdriver.Chrome()
+driver.get('https://sia.unal.edu.co/Catalogo/facespublico/public/servicioPublico.jsf?taskflowId=task-flow-AC_CatalogoAsignaturas')
+
+selectNivelEstudio = driver.find_element(by=By.ID, value='pt1:r1:0:soc1::content')
+selectNivelEstudio.send_keys('0')
